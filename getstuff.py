94502cf08f2208b68b712208d705ffc500c9b559
Isopach <kohyouliang@gmail.com>
2016-07-20 13:31:49 +0900

    updated for finals

--- getstuff.py
+++ getstuff.py
@@ -11,6 +11,7 @@ import json
 from scipy import misc, ndimage
 import numpy as np
 from PIL import Image
+import random
 br = mechanize.Browser()
 
 # cookie jar
@@ -37,13 +38,14 @@ nameslist = soup.findAll("span", { "class" : "last-name" })
 pointslist = soup.findAll("td", { "class" : "points" }) 
 #open the file to keep the list, as required
 fo = open('url.txt', 'w')
-logf = open('log.txt', 'w')   
+logf = open('log.txt', 'w')  
+logz = open('selected_urls.txt', 'w')   
 htmlf = open('index.html', 'w')
 #index.html
 html_top = """
 <html>
 <head>
-    <title>My Home Page: Midterm</title>
+    <title>ホームページ・期末課題</title>
 </head>
 
 <body>
@@ -54,25 +56,32 @@ html_bot = """
 """
 htmlf.write(html_top)
 
-
 #Url list
 for img in imagelist:
         print "".join(["http://www.formula1.com",img['src']])
         fo.write("".join(["http://www.formula1.com",img['src']])+"\n")
 
 fo.close()
-
-jfile = open('list.json')
+jfile = open('list.json') 
 jfile_str = jfile.read()
-jdata = jfile_str.splitlines()
-countj = 0
-userj = input("Enter an integer: ")
-if (isinstance(userj, int) == True):
-    if userj < len(jdata):
+jdata = jfile_str.splitlines() #opens as list (R1)
+def img_scrapper():
+    countj = 0
+    userj = input("Enter an integer: ")
+    if (isinstance(userj, int) == True):
+        try:
+            if userj < len(jdata):
+                userj = userj
+            else:
+                userj =  random.randint(1, (len(jdata)-1))
+                print "Index exceeded. A random number has been generated for you: %s" % userj
+        #Catches error and allows for index to be exceeded via random number generation
+        except (TypeError, ValueError, IndexError):
+            pass
         while countj < userj:
             print jdata[countj]
-            logf.write(jdata[countj]+"\n")
-            #if address is jpg
+            logz.write(jdata[countj]+"\n")
+            #if address is jpg (split string based on regex /)
             if jdata[countj].rsplit('/', 1)[-1].endswith('jpg'):
                 savedfile = 'rotated_' + jdata[countj].rsplit('/', 1)[-1]
                 os.system('wget64 %s -O %s' % (jdata[countj], savedfile))
@@ -90,20 +99,65 @@ if (isinstance(userj, int) == True):
                 os.system('wget64 %s -O %s' % (jdata[countj], jdata[countj].rsplit('/', 1)[-1])) #downloads file; requires installation of wget or wget64 in this case
                 htmlf.write("<a href='" + jdata[countj] + "'></a>" + "\n")
                 countj += 1 
+            
         
-    else:
-        print "Please enter an integer smaller than %s" % len(jdata)
+#Takes data from multiple (2) webpages and concentates them within one dict, while merging points from identical racers
+url = ("http://www.formula1.com/content/fom-website/en/championship/results/2016-race-results/")
+def racers(url):
+    racersDict = {}
+    payload = ["2016-australia-results/race.html", "2016-bahrain-results/race.html"]
+    x = 0
+    while (x < len(payload)):
+        rx = br.open(url+payload[x])
+        htmlx = rx.read()
+        soupx = bsoup(html)
 
-#Names and points
-for eachname, eachpoint in zip(nameslist, pointslist):
-   names=eachname.next
-   points = eachpoint.next
-   if (str(type(names)) != "<class 'BeautifulSoup.Tag'>"):
-      
-      print names.string, "\t", points.string
-      logf.write(names.string+"\t"+points.string+"\n")
+        urlslist = soup.findAll("span", { "class" : "last-name" })
+        imagelist = soup.findAll("img")
+        nameslist = soup.findAll("span", { "class" : "last-name" })
+        pointslist = soup.findAll("td", { "class" : "points" }) 
+        
+        #Names and points
+        for eachname, eachpoint in zip(nameslist, pointslist):
+           names=eachname.next
+           points = eachpoint.next
+           if (str(type(names)) != "<class 'BeautifulSoup.Tag'>"):
+           #Checks if name is already in dictionary and adds the points if so
+              if names.string in racersDict:
+                racersDict[names.string] = int(racersDict[names]) + int(points)
+              else:
+                racersDict[names.string] = points.string
+           #print racersDict
+           for i in racersDict.iterkeys():
+              logf.write(str(i)+"\t"+str(racersDict[i])+"\n")
+            
+        x += 1
       
 jfile.close()
 logf.close()
 htmlf.write(html_bot)
 htmlf.close()
+
+if __name__ == '__main__':
+    fo = open('url.txt', 'w')
+    logf = open('log.txt', 'w')  
+    logz = open('selected_urls.txt', 'w')   
+    htmlf = open('index.html', 'w')
+    htmlf.write(html_top)
+    img_scrapper()
+    jfile.close()
+    logf.close()
+    htmlf.write(html_bot)
+    htmlf.close()
+    logf = open('log.txt', 'w')
+    racers(url)
+    logf.close()
+    logx = open('log.txt', 'r')
+    excess_data = logx.readlines()
+    #Deletes duplicate data (extremely hacky - will fix in future)
+    del excess_data[0:710]
+    del excess_data[22:]
+    logx.close()
+    logf = open('log.txt', 'w')
+    logf.writelines(excess_data)
+    logf.close()
